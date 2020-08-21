6ca7772cc2b60e1475ef1fba55c79cec073c0016
Jeremy Fleshman <jeremy.fleshman@icloud.com>
2020-08-21 16:46:51 -0500

    Initial Commit

---
+++ HWS_100DoS_Day59ChallengeApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HWS_100DoS_Day59ChallengeApp
+//
+//  Created by Jeremy Fleshman on 8/21/20.
+//  Copyright © 2020 Jeremy Fleshman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
