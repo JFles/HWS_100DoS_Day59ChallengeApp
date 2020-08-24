8dfb328e94398601cb5af91858f0cb7285b51b99
Jeremy Fleshman <jeremy.fleshman@icloud.com>
2020-08-24 14:10:58 -0500

    - Added scaffolded data model Countries and Country - Added example JSON from restcountries.eu open source API

---
+++ HWS_100DoS_Day59ChallengeApp/Models/Countries.swift
@@ -0,0 +1,13 @@
+//
+//  Countries.swift
+//  HWS_100DoS_Day59ChallengeApp
+//
+//  Created by Jeremy Fleshman on 8/24/20.
+//  Copyright © 2020 Jeremy Fleshman. All rights reserved.
+//
+
+import Foundation
+
+struct Countries {
+    #warning("Implement")
+}
---
+++ HWS_100DoS_Day59ChallengeApp/Models/Country.swift
@@ -0,0 +1,13 @@
+//
+//  Country.swift
+//  HWS_100DoS_Day59ChallengeApp
+//
+//  Created by Jeremy Fleshman on 8/24/20.
+//  Copyright © 2020 Jeremy Fleshman. All rights reserved.
+//
+
+import Foundation
+
+struct Country {
+    #warning("Implement")
+}
