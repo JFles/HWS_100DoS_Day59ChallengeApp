2a571426c632587009062b5db0df4d751b705cfb
Jeremy Fleshman <jeremy.fleshman@icloud.com>
2020-08-25 16:55:50 -0500

    - Improved data model to handle missing keys in api network call - Corrected data type for `area` key

--- HWS_100DoS_Day59ChallengeApp/Models/Country.swift
+++ HWS_100DoS_Day59ChallengeApp/Models/Country.swift
@@ -13,23 +13,45 @@ struct Country: Decodable {
         var code: String
         var name: String
         var symbol: String
+
+        private enum CurrencyCodingKeys: String, CodingKey {
+            case code
+            case name
+            case symbol
+        }
+
+        init(from decoder: Decoder) throws {
+            let container = try decoder.container(keyedBy: CurrencyCodingKeys.self)
+            code = try container.decodeIfPresent(String.self, forKey: .code) ?? ""
+            name = try container.decodeIfPresent(String.self, forKey: .name) ?? ""
+            symbol = try container.decodeIfPresent(String.self, forKey: .symbol) ?? ""
+        }
     }
 
     struct Language: Decodable {
         var name: String
+
+        private enum LanguageCodingKeys: String, CodingKey {
+            case name
+        }
+
+        init(from decoder: Decoder) throws {
+            let container = try decoder.container(keyedBy: LanguageCodingKeys.self)
+            name = try container.decodeIfPresent(String.self, forKey: .name) ?? ""
+        }
     }
 
     var name: String
     var capital: String
     var population: Int
     var demonym: String // i.e. demonym of "Spain" = "Spaniard"
-    var area: Int // FIXME: this key breaks the decode
+    var area: Double
     var timezones: [String]
-    var currencies: [Currency] // FIXME: this key breaks the decode
+    var currencies: [Currency]
     var languages: [Language]
     var flagUrl: String
     
-    private enum CodingKeys: String, CodingKey {
+    private enum CountryCodingKeys: String, CodingKey {
         case name
         case capital
         case population
@@ -42,16 +64,17 @@ struct Country: Decodable {
     }
 
     init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        name = try container.decode(String.self, forKey: .name)
-        capital = try container.decode(String.self, forKey: .capital)
-        population = try container.decode(Int.self, forKey: .population)
-        demonym = try container.decode(String.self, forKey: .demonym)
-        area = try container.decodeIfPresent(Int.self, forKey: .area) ?? -1
-        timezones = try container.decode([String].self, forKey: .timezones)
+        #warning("Evaluate decoder for non optional values")
+        let container = try decoder.container(keyedBy: CountryCodingKeys.self)
+        name = try container.decodeIfPresent(String.self, forKey: .name) ?? ""
+        capital = try container.decodeIfPresent(String.self, forKey: .capital) ?? ""
+        population = try container.decodeIfPresent(Int.self, forKey: .population) ?? -1
+        demonym = try container.decodeIfPresent(String.self, forKey: .demonym) ?? ""
+        area = try container.decodeIfPresent(Double.self, forKey: .area) ?? 10
+        timezones = try container.decodeIfPresent([String].self, forKey: .timezones) ?? [String]()
         currencies = try container.decodeIfPresent([Currency].self, forKey: .currencies) ?? [Currency]()
-        languages = try container.decode([Language].self, forKey: .languages)
-        flagUrl = try container.decode(String.self, forKey: .flagUrl)
+        languages = try container.decodeIfPresent([Language].self, forKey: .languages) ?? [Language]()
+        flagUrl = try container.decodeIfPresent(String.self, forKey: .flagUrl) ?? ""
     }
 }
 
