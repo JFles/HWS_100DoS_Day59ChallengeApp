bd93d6f2b38c9b0ee55e6af483be97f34b893dcc
Jeremy Fleshman <jeremy.fleshman@icloud.com>
2020-08-25 09:51:26 -0500

    - Renamed sample json file for length - Finished implementing CodingKeys and custom decoder init for Country data model

--- HWS_100DoS_Day59ChallengeApp/Models/Country.swift
+++ HWS_100DoS_Day59ChallengeApp/Models/Country.swift
@@ -8,37 +8,34 @@
 
 import Foundation
 
-struct Country: Codable {
-    #warning("Refactor data model with coding keys for better intent")
-    struct Currency: Codable {
+struct Country: Decodable {
+    struct Currency: Decodable {
         var code: String
         var name: String
         var symbol: String
     }
-//
-//    struct Language: Codable {
-//        var name: String
-//    }
+
+    struct Language: Decodable {
+        var name: String
+    }
 
     var name: String
     var capital: String
     var population: Int
     var demonym: String // i.e. demonym of "Spain" = "Spaniard"
     var area: Int // FIXME: this key breaks the decode
-//    var timeZones: [String]
+    var timezones: [String]
     var currencies: [Currency] // FIXME: this key breaks the decode
-//    var languages: [Language]
-    var flag: String
-
-    #warning("Add an init and coding keys to have optional keys")
-    /// https://medium.com/flawless-app-stories/lets-parse-the-json-like-a-boss-with-swift-codable-protocol-3d4c4290c104
+    var languages: [Language]
+    var flagUrl: String
+    
     private enum CodingKeys: String, CodingKey {
         case name
         case capital
         case population
         case demonym
         case area
-//        case timeZones
+        case timezones
         case currencies
         case languages
         case flagUrl = "flag"
@@ -51,8 +48,10 @@ struct Country: Codable {
         population = try container.decode(Int.self, forKey: .population)
         demonym = try container.decode(String.self, forKey: .demonym)
         area = try container.decodeIfPresent(Int.self, forKey: .area) ?? -1
+        timezones = try container.decode([String].self, forKey: .timezones)
         currencies = try container.decodeIfPresent([Currency].self, forKey: .currencies) ?? [Currency]()
-        flag = try container.decode(String.self, forKey: .flagUrl)
+        languages = try container.decode([Language].self, forKey: .languages)
+        flagUrl = try container.decode(String.self, forKey: .flagUrl)
     }
 }
 
